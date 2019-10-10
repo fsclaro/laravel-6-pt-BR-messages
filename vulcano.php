0dcbc9f2b7c7914355076af0ca35fe008b5b4fe4
Fernando <fsclaro@gmail.com>
2019-10-10 14:19:04 -0300

    lang pt-br

---
+++ vulcano.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    'today'             => 'Hoje',
+    'profile'           => 'Perfil',
+    'all'               => 'Todos',
+    'yes'               => 'Sim',
+    'no'                => 'Não',
+    'update'            => 'Atualizar a tela',
+    'insert'            => 'Inserir um novo registro',
+    'atention'          => 'Atenção',
+    'confirm_delete'    => 'Deseja realmente excluir este registro?',
+    'view_details'       => 'Visualizar detalhes',
+
+    // Usuários
+    'users' => [
+        'listing'       => 'Listar Usuários',
+        'title'     => 'Relação de Usuários do Sistema',
+        'id'        => 'ID',
+        'user_name' => 'Usuário',
+        'email'     => 'E-mail',
+        'role'      => 'Papel',
+        'active'    => 'Ativo?',
+        'avatar'    => 'Avatar',
+        'actions'   => 'Ações',
+    ],
+
+];
